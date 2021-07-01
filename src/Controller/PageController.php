df36fb5fa8bdeb65fb527944d84a02a3d4d4203f
mia <mia.nova33@gmail.com>
2021-07-01 14:28:26 +0200

    poker commit

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,4 +23,58 @@ class PageController
     {
         return new Response('Mentions Légales');
     }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+
+        // j'utilise la classe Request du composant HTTPFoundation
+        // et la méthode createFromGlobals qui met permet de récupérer
+        // tous les parametre GET / POST etc
+        $request = Request::createFromGlobals();
+
+        // je stocke dans une variable $request la valeur
+        // du parametre GET 'sent'
+        $sent = $request->query->get('sent');
+
+        // si le parametre GET 'sent' est égal à 'yes' alors j'envoie
+        // une réponse avec 'merci pour le form'
+        if ($sent === 'yes') {
+            return new Response("merci pour le formulaire");
+            // sinon je renvoie le formulaire en réponse
+        } else {
+            return new Response("formulaire");
+        }
+
+    }
+
+    /**
+     * @Route("/poker", name="poker")
+     */
+    public function poker()
+    {
+
+        // j'utilise la classe Request du composant HTTPFoundation
+        // et la méthode createFromGlobals qui met permet de récupérer
+        // tous les parametre GET / POST etc
+        $request = Request::createFromGlobals();
+
+        // je stocke dans une variable $request la valeur
+        // du parametre GET 'sent'
+        $age = $request->query->get('age');
+
+        // si le parametre GET 'sent' est égal à 'yes' alors j'envoie
+        // une réponse avec ''
+        if ($age > 18) {
+            return new Response("Bienvenue chez Winalooz");
+            // sinon je renvoie le formulaire en réponse
+        }
+            return new Response("Vazy bouge de là");
+
+
+    }
+
+
+
 }
